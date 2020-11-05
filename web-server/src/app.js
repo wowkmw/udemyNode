d7a9c2c69515146fbe768421ffebbce9d4474f0d
Mu Wei Kuo <kmw03062@hotmail.com>
2020-11-05 23:14:33 +1000

    basic wer server setup

---
+++ web-server/src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+// app.com
+// app.com/help
+
+app.get('', (req, res) => {
+    res.send('<h1>Hello express</h1>');
+});
+
+app.get('/help', (req, res) => {
+    res.send('<h1>Help page</h1>');
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About page</h1>');
+});
+
+app.get('/weather', (req, res) => {
+    res.send('<h1>Weather page</h1>');
+});
+
+app.listen(3000, () => console.log('server is up on port 3000...'));
