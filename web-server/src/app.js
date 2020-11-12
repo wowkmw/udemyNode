b6392d858312d8902af82d94d92a510c619e410f
Mu Wei Kuo <kmw03062@hotmail.com>
2020-11-12 22:19:22 +1000

    heroku setup complete

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,6 +5,7 @@ const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //define paths for express configuration
 const publicDir = path.join(__dirname, '../public');
@@ -108,4 +109,4 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('server is up on port 3000...'));
+app.listen(port, () => console.log(`server is up on port ${port}...`));
